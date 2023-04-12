5d0d53ca7d1d4fe29472bbaae0a8eccc4e9ff6a8
Anton Kochekov <95627984+avkochekov@users.noreply.github.com>
2023-04-12 21:57:05 +0600

    Add anonymous class and lambda usage example

--- app/src/main/java/av/kochekov/playlistmaker/MainActivity.kt
+++ app/src/main/java/av/kochekov/playlistmaker/MainActivity.kt
@@ -3,6 +3,7 @@ package av.kochekov.playlistmaker
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
@@ -14,8 +15,33 @@ class MainActivity : AppCompatActivity() {
         var libraryButton = findViewById<Button>(R.id.menu_library)
         var settingsButton = findViewById<Button>(R.id.menu_settings)
 
+        // Using lambdas - v.1 =====================================================================
         searchButton.setOnClickListener{startActivity(Intent(this, SearchActivity::class.java))}
         libraryButton.setOnClickListener{startActivity(Intent(this, LibraryActivity::class.java))}
         settingsButton.setOnClickListener{startActivity(Intent(this, SettingsActivity::class.java))}
+
+//        // Using lambdas - v.2 =====================================================================
+//        var onClickListner: View.OnClickListener = View.OnClickListener { v ->
+//            when (v?.id ?: -1) {
+//                R.id.menu_search -> startActivity(Intent(this@MainActivity, SearchActivity::class.java))
+//                R.id.menu_library -> startActivity(Intent(this@MainActivity, LibraryActivity::class.java))
+//                R.id.menu_settings -> startActivity(Intent(this@MainActivity, SettingsActivity::class.java))
+//            }
+//        }
+
+//        // Using anonymous classes =================================================================
+//        var onClickListner: View.OnClickListener = object : View.OnClickListener {
+//            override fun onClick(v: View?) {
+//                when (v?.id ?: -1) {
+//                    R.id.menu_search -> startActivity(Intent(this@MainActivity, SearchActivity::class.java))
+//                    R.id.menu_library -> startActivity(Intent(this@MainActivity, LibraryActivity::class.java))
+//                    R.id.menu_settings -> startActivity(Intent(this@MainActivity, SettingsActivity::class.java))
+//                }
+//            }
+//        }
+//        searchButton.setOnClickListener(onClickListner)
+//        libraryButton.setOnClickListener(onClickListner)
+//        settingsButton.setOnClickListener(onClickListner)
+
     }
 }
