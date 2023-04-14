14538581e70d636d014b67fb642a1b9e775f5f7c
Anton Kochekov <95627984+avkochekov@users.noreply.github.com>
2023-04-14 08:22:41 +0600

    Fix reviewer comments

--- app/src/main/java/av/kochekov/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/av/kochekov/playlistmaker/SettingsActivity.kt
@@ -2,15 +2,14 @@ package av.kochekov.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
+import androidx.appcompat.widget.Toolbar
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
-
-        findViewById<ImageView>(R.id.back).setOnClickListener {
-            this.finish()
+        findViewById<Toolbar>(R.id.settings_toolbar).setNavigationOnClickListener {
+            onBackPressed()
         }
     }
 }
