d97e2bd247b71a5acbabc1e40980d8e08e14489b
Anton Kochekov <95627984+avkochekov@users.noreply.github.com>
2023-04-20 09:48:30 +0600

    Add base search activity logic

--- app/src/main/java/av/kochekov/playlistmaker/SearchActivity.kt
+++ app/src/main/java/av/kochekov/playlistmaker/SearchActivity.kt
@@ -2,10 +2,46 @@ package av.kochekov.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.appcompat.widget.Toolbar
+import androidx.core.widget.addTextChangedListener
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+        findViewById<Toolbar>(R.id.toolbar).setNavigationOnClickListener {
+            onBackPressed()
+        }
+        val inputEditText = findViewById<EditText>(R.id.search_query)
+        val clearButton = findViewById<ImageView>(R.id.search_clear)
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) { }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = clearButtonVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) { }
+        }
+        inputEditText.addTextChangedListener(simpleTextWatcher)
+
+        clearButton.setOnClickListener {
+            inputEditText.setText("")
+        }
     }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
+
 }
