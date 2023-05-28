5c76cd83a8a9847aeefdc333521b558c0b102096
Anton Kochekov <95627984+avkochekov@users.noreply.github.com>
2023-05-28 11:27:43 +0600

    Add Search history class

---
+++ app/src/main/java/av/kochekov/playlistmaker/SearchHistory.kt
@@ -0,0 +1,35 @@
+package av.kochekov.playlistmaker
+
+import android.content.SharedPreferences
+import com.google.gson.Gson
+
+class SearchHistory(private val pref: SharedPreferences) {
+    companion object{
+        const val DATA_KEY = "TRACK_LIST_HISTORY"
+    }
+
+    fun add(track: Track){
+        var trackList = get().toMutableList()
+        trackList.remove(track)
+        if (trackList.size == 10)
+            trackList.removeFirst()
+        trackList.add(track)
+        pref.edit()
+            .putString(DATA_KEY, Gson().toJson(trackList))
+            .apply()
+    }
+
+
+
+    fun get(): List<Track> {
+        return pref.getString(DATA_KEY, null)
+            ?.let {
+                return Gson().fromJson(it, Array<Track>::class.java).toList()
+            }
+            ?: emptyList()
+    }
+
+    fun clear(){
+        pref.edit().clear()
+    }
+}
