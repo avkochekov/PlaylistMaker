e0e51e2c1b8671495d6174478e94820f96b39a9c
Anton Kochekov <95627984+avkochekov@users.noreply.github.com>
2023-05-28 11:28:08 +0600

    Update track list adapter class

--- app/src/main/java/av/kochekov/playlistmaker/TrackListAdapter.kt
+++ app/src/main/java/av/kochekov/playlistmaker/TrackListAdapter.kt
@@ -1,10 +1,12 @@
 package av.kochekov.playlistmaker
 
+import android.content.SharedPreferences
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import org.jetbrains.annotations.Nullable
 
-class TrackListAdapter : RecyclerView.Adapter<TrackListHolder> () {
+class TrackListAdapter(@Nullable var preferences: SharedPreferences? = null) : RecyclerView.Adapter<TrackListHolder> () {
     private var data = mutableListOf<Track>()
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackListHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.track_list_item, parent, false)
@@ -13,6 +15,9 @@ class TrackListAdapter : RecyclerView.Adapter<TrackListHolder> () {
 
     override fun onBindViewHolder(holder: TrackListHolder, position: Int) {
         holder.bind(data[position])
+        holder.itemView.setOnClickListener {
+            preferences?.let { SearchHistory(preferences!!).add(data[position]) }
+        }
     }
 
     override fun getItemCount(): Int {
