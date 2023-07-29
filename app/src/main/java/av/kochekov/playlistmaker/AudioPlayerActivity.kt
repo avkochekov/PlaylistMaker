7d4b7d5c8b6de72ab60b3ed7334b94ced535a275
Anton Kochekov <95627984+avkochekov@users.noreply.github.com>
2023-07-29 16:19:25 +0600

    Remove unneeded imports

--- app/src/main/java/av/kochekov/playlistmaker/AudioPlayerActivity.kt
+++ app/src/main/java/av/kochekov/playlistmaker/AudioPlayerActivity.kt
@@ -4,12 +4,10 @@ import android.media.MediaPlayer
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.AppCompatImageButton
 import androidx.appcompat.widget.Toolbar
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
--- app/src/main/java/av/kochekov/playlistmaker/SearchActivity.kt
+++ app/src/main/java/av/kochekov/playlistmaker/SearchActivity.kt
@@ -3,7 +3,6 @@ package av.kochekov.playlistmaker
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
-import android.content.SharedPreferences
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener
 import android.os.Bundle
 import android.os.Handler
@@ -11,7 +10,6 @@ import android.os.Looper
 import android.os.Looper.*
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
